d6223119ebb9e40c7fe8d5a573ed3c3519a77179
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2023-07-12 20:12:05 +0000

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+game "common"
+
+name "cfx-i18next"
+version "1.0.0"
+description "i18next wrapper for FXServer"
+license "LGPL-3.0-or-later"
+author "r3ps4J"
+repository "https://github.com/r3ps4J/cfx-i18next"
+
+shared_script "dist/index.js"
