ce8493534852d1ef5b4918c6305d784481a83399
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2023-07-20 22:32:37 +0200

    Add language detector

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import i18next from "i18next";
+import languageDetector from "@r3ps4j/i18next-cfx-language-detector";
 
 const exports = globalThis.exports;
 
 exports("createInstance", i18next.createInstance);
+
+function getLanguageDetector() {
+    return languageDetector;
+}
+
+exports("getLanguageDetector", getLanguageDetector);
