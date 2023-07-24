fd292184b5001acb021b8758be283aa955d4fa0d
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2023-07-24 21:57:44 +0200

    Update for language detector class

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import i18next from "i18next";
-import languageDetector from "@r3ps4j/i18next-cfx-language-detector";
+import i18next, { Callback, InitOptions, i18n } from "i18next";
+import LanguageDetector from "@r3ps4j/i18next-cfx-language-detector";
 
 const exports = globalThis.exports;
 
 exports("createInstance", i18next.createInstance);
 
-function getLanguageDetector() {
-    return languageDetector;
+function createInstanceWithPlugins(
+    plugins: "languageDetector"[] = ["languageDetector"],
+    options?: InitOptions<object>,
+    callback?: Callback
+): i18n {
+    const newInstance = i18next.createInstance(options, callback);
+    if (plugins.includes("languageDetector")) {
+        newInstance.use(LanguageDetector);
+    }
+    return newInstance;
 }
 
-exports("getLanguageDetector", getLanguageDetector);
+exports("createInstanceWithPlugins", createInstanceWithPlugins);
