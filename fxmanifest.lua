ac4e2297aa636f80bedb99345f77043f4a9a9d5a
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2023-08-14 15:38:00 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,14 @@
+-- FX information
 fx_version "cerulean"
 game "common"
 
+-- Resource information
 name "cfx-i18next"
 version "1.0.0"
 description "i18next wrapper for FXServer"
-license "LGPL-3.0-or-later"
 author "r3ps4J"
+license "LGPL-3.0-or-later"
 repository "https://github.com/r3ps4J/cfx-i18next"
 
+-- Manifest
 shared_script "dist/index.js"
